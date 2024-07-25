acfadada804283a45fcdcbee1b2210c0b5c84014
kihyun_pc <mable0927@gmail.com>
2024-07-26 00:02:36 +0900

    FEAT/STORE: 판매자 관련 모델 작성

--- thirtyone/store/admin.py
+++ thirtyone/store/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Store
+from .models import Store,Order,StoreType
 # Register your models here.
 
+#어드민페이지에서 수정할 것들 추가
 admin.site.register(Store)
+admin.site.register(Order)
+admin.site.register(StoreType)
