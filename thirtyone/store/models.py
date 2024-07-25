06661df1dd0b0ce4620aa06461f29ac3e3cdd87d
kihyun_pc <mable0927@gmail.com>
2024-07-26 00:14:45 +0900

    FIX: base파일에서 BASE_DIR 코드오류로 인한 실행 안됨 문제 해결

--- thirtyone/store/models.py
+++ thirtyone/store/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Store(models.Model): # 판매자 모델 정의
-    store_name = models.CharField(max_length=50) # 가게 이름
-    store_photo = models.ImageField(upload_to='store_phothos/') # 가게 사진
+    name = models.CharField(max_length=50) # 가게 이름
+    photo = models.ImageField(upload_to='store_phothos/') # 가게 사진
     address = models.CharField(max_length=255) #가게 주소
     open_time = models.TimeField() # 가게 오픈 시간
     close_time = models.TimeField() # 가게 마감시간
