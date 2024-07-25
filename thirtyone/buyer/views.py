87aaa3eb6de26b0927ecb1f23e618cad60f280d8
kihyun_pc <mable0927@gmail.com>
2024-07-25 21:58:05 +0900

    COMMENT: 주석추가

--- thirtyone/buyer/views.py
+++ thirtyone/buyer/views.py
@@ -5,6 +5,7 @@ from .serializers import BuySerializer
 
 # Create your views here.
 
+#구매자 회원 시작 
 class BuyerCreateView(generics.CreateAPIView): #제너릭 뷰 사용함
     queryset = Buyer.objects.all() # 모든 구매자 객체
     serializer_class = BuySerializer #구매자 시리얼라이저 사용
