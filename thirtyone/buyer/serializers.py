2ed3f146a882423fcd1ed1acec2f27a60797c1ae
kihyun_pc <mable0927@gmail.com>
2024-07-26 15:14:39 +0900

    FEAT/BUYER: 떨이 픽 개발 중 임시 저장

--- thirtyone/buyer/serializers.py
+++ thirtyone/buyer/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Buyer
+from store.models import * #store의 모델 가져옴
 
 #구매자 시작 시리얼 라이저
 class BuySerializer(serializers.ModelSerializer): 
@@ -7,4 +8,12 @@ class BuySerializer(serializers.ModelSerializer):
         model = Buyer
         fields = ['id','name']
 
+#주문서 작성 시리얼 라이저
+class OrderCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['']
 
+#주문서 반환 시리얼라이저
+class OrderDeailSerializer(serializers.ModelSerializer):
+    class Meta:
