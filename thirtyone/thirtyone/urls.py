3b77bf3baf592c3a4215b1b20c33de9f994d8fb6
kihyun <mable0927@gmail.com>
2024-07-31 19:27:21 +0900

    Docs:스웨거 기본 설정 추가

--- thirtyone/thirtyone/urls.py
+++ thirtyone/thirtyone/urls.py
@@ -1,8 +1,28 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.urls import re_path
+from rest_framework.permissions import AllowAny
+from drf_yasg.views import get_schema_view
+from drf_yasg       import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="thirtyone-project",
+        default_version='1.0',
+        description="thirtyone-project API 문서)",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="likelion@inha.edu"), # 부가정보
+        license=openapi.License(name="backend"),     # 부가정보
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('buyer/', include('buyer.urls')), # buyer 앱의 URL 패턴 포함
-    path('store/', include('store.urls')) # store 앱 url 연결
+    path('store/', include('store.urls')), # store 앱 url 연결
+    # Swagger url
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
