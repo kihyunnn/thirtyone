3701691b1db0e246fa4a975ae796bd93ae371c06
dasol <5dasoll37@gmail.com>
2024-07-31 17:05:47 +0900

    Refactor: models.py/Order save() accept_at 업데이트 코드를 views.py에서 직접하는 걸로 변경

--- thirtyone/store/views.py
+++ thirtyone/store/views.py
@@ -4,7 +4,6 @@ from .serializers import *
 from .models import *
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .tasks import check_order_status
 
 # 가게 등록
 @api_view(['POST'])
@@ -76,18 +75,24 @@ def order_update(request, pk, order_id):
     if serializer.is_valid():
         sale_product_name = order.sale_product
         sale_product = SaleProduct.objects.get(store=store, name=sale_product_name)
-        if serializer.validated_data.get('buy_step') == Order.OrderStepCategory.PICKUP_PEND:
-            order.accept_at = timezone.now()
-            sale_product.amount -= order.amount # 주문수락하면 떨이 수량 감소시키기
-            sale_product.save()
-            #check_order_status.apply_async(countdown=1800)  # 30분 후 실행
 
-        if serializer.validated_data.get('buy_step') == Order.OrderStepCategory.PICUP_COMP:
+        if serializer.validated_data.get('buy_step') == Order.OrderStepCategory.PICKUP_PEND: # 주문수락
+            order.accept_at = timezone.now()
+            order.reject_at = order.accept_at + timedelta(minutes=30) # 수락시간 + 30분 = 자동 주문 취소 시간
+            sale_product.amount -= order.amount # 주문수락하면 떨이 수량 감소시키기
+            order.save()
+            sale_product.save()
+
+        if serializer.validated_data.get('buy_step') == Order.OrderStepCategory.PICUP_COMP: # 픽업완료
             today = datetime.date.today()
             sale_record = SaleRecord.objects.get(date=today, sale_product=sale_product)
             sale_record.selled_amount += order.amount
             sale_record.save()
+
+        if serializer.validated_data.get('buy_step') == Order.OrderStepCategory.AUTO_CANCEL: # 주문 자동취소
+            sale_product.amount += order.amount # 자동취소되면 해당 수량 다시 떨이상품에 업데이트 시킴
+            sale_product.save()
+
         serializer.save()
         return Response(serializer.data, status=200)
     return Response(serializer.errors, status=400)
-
